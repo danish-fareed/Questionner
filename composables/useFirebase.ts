9994d9c01eb55adadfbeb8b11f554f8f3abeb950
Danish Fareed <104269116+danish-fareed@users.noreply.github.com>
2022-12-12 22:07:45 +0500

    Entry Part 1

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -4,12 +4,13 @@ import { getAuth } from "firebase/auth";
 
 export const useFirebase = () => {
     const firebaseConfig = {
-        apiKey: "AIzaSyA-zFOEYNNXV9InRIeRaBVXOm7kPiAmql4",
-        authDomain: "questionner-9015b.firebaseapp.com",
-        projectId: "questionner-9015b",
-        storageBucket: "questionner-9015b.appspot.com",
-        messagingSenderId: "389546612732",
-        appId: "1:389546612732:web:b264a50efa7b07d61bc479"
+      apiKey: "AIzaSyACK5ShG7H_GOu3-IfLi3gNa7p73tL6SQw",
+      authDomain: "questionner-795c5.firebaseapp.com",
+      projectId: "questionner-795c5",
+      storageBucket: "questionner-795c5.appspot.com",
+      messagingSenderId: "308137162545",
+      appId: "1:308137162545:web:61b1d428ed5b926b776210",
+      measurementId: "G-4LPRXNE8YB"
     };
 
     const firebaseApp = initializeApp(firebaseConfig);
